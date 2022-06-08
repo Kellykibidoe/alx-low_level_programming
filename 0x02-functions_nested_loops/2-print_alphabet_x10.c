2f5ea3ac409b689fcd459e5764308d7ff431ebc6
Kelly Kibidoe <kelly.kibidoe@gmail.com>
2022-06-08 12:32:15 -0700

    correction on spaces

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 
 void print_alphabet(void)
 {
-        int j;
+	int j;
 	int x;
 
-        for (x = 0; j < 10; x++)
-        {
+	for (x = 0; j < 10; x++)
+	{
 		for (j = 'a'; j<= 'z'; j++)
 		{
 			_putchar(j);
-        	}
-        	_putchar('\n');
+		}
+		_putchar('\n');
 }
