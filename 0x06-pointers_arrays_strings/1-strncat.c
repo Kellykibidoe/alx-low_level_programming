a9b4109057849c48dfab53a8d8a4c0bbc21a00f9
Kelly Kibidoe <kelly.kibidoe@gmail.com>
2022-06-17 10:28:35 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i = 0;
-	int j = 0;
+	int dlen = 0, j = 0;
 
-	while (*(dest + i) != '\0')
-		i++;
-	while (*(src + j) != '\0')
+	while (dest[dlen])
 	{
-		if (j < n)
-		{
-		i	*(dest + i) = *(src + j);
-			i++;
-		}
+		dlen++;
+	}
+	
+	while (j < n && src[j])
+	{
+		dest[dlen] = src[j];
+		dlen++;
 		j++;
 	}
+	dest[dlen + n + 1] = '\0';
 	return (dest);
 }
