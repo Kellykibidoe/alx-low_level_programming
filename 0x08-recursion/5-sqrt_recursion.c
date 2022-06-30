e8c5ae7335b9c6e2e4dc4ede005ad21434259d7e
Kelly Kibidoe <kelly.kibidoe@gmail.com>
2022-06-30 15:39:50 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,7 +19,7 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * getNumSqr - function that gets the num of sqroot
+ * _sqrt - function that gets the num of sqroot
  * @num: input number
  * @i: Counter variable
  * Return: int
